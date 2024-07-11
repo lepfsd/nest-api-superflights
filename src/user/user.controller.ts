1cb82b3101dc1187fce60697efb5cfaed964042c
fsd <lepfsd00@gmail.com>
2024-07-11 09:28:54 -0400

    feat - users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { UserDTO } from './dto/user.dto';
 import { UserService } from './user.service';
 
@@ -9,5 +17,25 @@ export class UserController {
   @Post()
   create(@Body() userDTO: UserDTO) {
     return this.userService.create(userDTO);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() userDTO: UserDTO) {
+    return this.userService.update(id, userDTO);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.userService.delete(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { UserDTO } from './dto/user.dto';
 import { IUser } from 'src/common/interfaces/user.interface';
 import * as bcrypt from 'bcrypt';
@@ -19,5 +19,24 @@ export class UserService {
     const hash = await this.hashPassword(userDTO.password);
     const newUser = new this.model({ ...userDTO, password: hash });
     return await newUser.save();
+  }
+
+  async findAll(): Promise<IUser[]> {
+    return await this.model.find();
+  }
+
+  async findOne(id: string): Promise<IUser> {
+    return await this.model.findById(id);
+  }
+
+  async update(id: string, userDTO: UserDTO): Promise<IUser> {
+    const hash = await this.hashPassword(userDTO.password);
+    const user = { ...userDTO, password: hash };
+    return await this.model.findByIdAndUpdate(id, user, { new: true });
+  }
+
+  async delete(id: string) {
+    await this.model.findByIdAndDelete(id);
+    return { status: HttpStatus.OK, msg: 'Deleted' };
   }
 }
